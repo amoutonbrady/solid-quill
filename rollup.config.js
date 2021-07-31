bf471d4fe7b296b0c3c221fd6b780d8683470024
Alexandre <amoutonbrady@gmail.com>
2021-07-31 12:31:24 +0200

    :wrench: Replace vite build with rollup-preset-solid

---
+++ rollup.config.js
@@ -0,0 +1,6 @@
+import withSolid from "rollup-preset-solid";
+
+export default withSolid({
+  input: "./src/solid-quill.tsx",
+  printInstructions: true,
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,6 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
-import { resolve } from "path";
 
 export default defineConfig({
   plugins: [solid()],
-  build: {
-    target: "esnext",
-    polyfillDynamicImport: false,
-    minify: false,
-    sourcemap: true,
-    lib: {
-      entry: resolve(__dirname, "src/solid-quill.tsx"),
-      formats: ["es", "cjs"],
-      name: "SolidQuill",
-    },
-    rollupOptions: {
-      external: ["solid-js", "solid-js/web", "quill"],
-    },
-  },
 });
